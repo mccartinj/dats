e272cec7f1009fe704abc5ddbf4f19c8ef1b2a26
John McCartin <jmccartin@Utile.local>
2019-06-12 15:39:09 -0400

    clean up, add notes

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const street_name = 'Heath';
 		const browser = await puppeteer.launch();
 		const page = await browser.newPage();
 
+
+		//navigate to page of links to properties' sdat cards
 		await page.goto(landing_url);
 		await page.select('#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucSearchType_ddlCounty','03');
 		await page.select('#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucSearchType_ddlSearchType','01');
@@ -22,48 +24,41 @@ const street_name = 'Heath';
 		await page.waitFor('input#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucEnterData_txtStreetName');
 		await page.$eval('input#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucEnterData_txtStreetName', (el, value) => el.value = value, street_name);
 		await page.click('input#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_StepNavigationTemplateContainerID_btnStepNextButton');
+		await page.waitFor('a#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucSearchResult_gv_SearchResult_lnkDetails_0');
 
-		await page.waitFor('a#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucSearchResult_gv_SearchResult_lnkDetails_89');
-
-		
+		//collect links and create an array to hold info from each card
 		const links = await page.$$('a.lnkdetails');
-
 		var all_data= [];
 
+		//loop through each link
 		for (let i = 0; i < links.length; i++) {
 			
 			let propdata = {};
 
+			//wait for last loop to finish
 			await page.waitFor(2000);
+
+			//click next link in line and wait for card to load
 			await page.click('a#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucSearchResult_gv_SearchResult_lnkDetails_'+i);
 			await page.waitFor(2000);
 
+			//throw sdat card into cheerio (node's version of jquery) for easy traversing
 			let content = await page.content()
 			var $ = cheerio.load(content);
 
-			//let premises_addrress = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_lblPremisesAddress_0').text();
-
+			//all addresses include a <br> tag, so we can't just use .text() ... instead put it in a node and run find and replace, then text() it.
 			let $address_node = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_lblPremisesAddress_0');
 			$address_node.find('br').replaceWith(' ');
 			let premises_addrress = $address_node.text();
-
-			//premises_addrress = premises_addrress.splice(premises_addrress.indexOf('BALTIMORE'),0," ")
-
-
 			propdata.premises_addrress = premises_addrress;
-			propdata.owner_name = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_lblOwnerName_0').text();
-
 
 			let $owner_mail_node = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_lblMailingAddress_0');
 			$owner_mail_node.find('br').replaceWith(' ');
 			let owner_mail = $owner_mail_node.text();
-			/*
-			if( owner_mail.indexOf('BALTIMORE') > 0 ) {
-				owner_mail = owner_mail.splice(owner_mail.indexOf('BALITMORE'),0," ");
-			}
-			*/
 			propdata.owner_mail = owner_mail;
 
+			//all the other fields we can just pull text() from for now. we may want to clean others (like gfa) later
+			propdata.owner_name = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_lblOwnerName_0').text();
 			propdata.prop_use = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_lblUse_0').text();
 			propdata.principal_resi = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_lblPrinResidence_0').text();
 			propdata.block = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_Label11_0').text();
@@ -86,6 +81,8 @@ const street_name = 'Heath';
 			propdata.nextlast_sale_price = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_Label46_0').text();
 			propdata.nextlast_seller = $('span#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_ucDetailsSearch_dlstDetaisSearch_Label44_0').text();
 
+			//this sort of verifies owner occupancy, but so far it's not working at all.
+			/*
 			let addr_match_mail = 'true';
 			if (propdata.premises_addrress == propdata.owner_mail) {
 				addr_match_mail = 'true';
@@ -94,23 +91,21 @@ const street_name = 'Heath';
 			}
 
 			propdata.addr_match_mail = addr_match_mail;
-
+			*/
 
 			console.log(premises_addrress);
-
-
 			all_data.push(propdata);
 
 			
 
 
-
+			//go back, but not with the browser's back button, since sdat isn't set up like that
 			await page.click('input#MainContent_MainContent_cphMainContentArea_ucSearchType_wzrdRealPropertySearch_btnPrevious_top2')
 		}
 
 
 
-		
+		//export to csv
 		let csv = new objectsToCsv(all_data);
 		await csv.toDisk('./test.csv',header=true)
 	  	await browser.close();
@@ -122,9 +117,3 @@ const street_name = 'Heath';
 
 })();
 
-
-
-
-String.prototype.splice = function(idx, rem, str) {
-    return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
-};
